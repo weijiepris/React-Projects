575732e91c60d77a74abfc8b3656e760af381667
weijiepris <altxmare@hotmail.com>
2021-09-13 19:00:39 +0800

    -

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,8 +1,21 @@
+import React from "react";
+import NavigationBar from "./components/NavigationBar";
+import classes from "./App.module.css";
+import { Switch } from "react-router-dom";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <Switch>
+      <React.Fragment>
+        <div className={classes.container}>
+          <NavigationBar />
+          <div className={classes.main}>
+            <h2>Sidenav Example</h2>
+            <p>This sidenav is always shown.</p>
+          </div>
+        </div>
+      </React.Fragment>
+    </Switch>
   );
 }
 
---
+++ portfolio/src/components/NavigationBar.js
@@ -0,0 +1,109 @@
+import React from "react";
+import classes from "./NavigationBar.module.css";
+import image from "../image/face.jpg";
+import linkedin from "../image/linkedin.png";
+import email from "../image/email.png";
+import { Link } from "react-router-dom";
+import { FaGithubSquare } from "@react-icons/all-files/fa/FaGithubSquare";
+import { FaLinkedin } from "@react-icons/all-files/fa/FaLinkedin";
+import { FaMailBulk } from "@react-icons/all-files/fa/FaMailBulk";
+
+function NavigationBar() {
+  return (
+    <React.Fragment>
+      <div className={classes.sidenav}>
+        <br></br>
+        <br></br>
+        <Link
+          to="/"
+          exact="true"
+          style={{ textDecoration: "none", marginTop: "none" }}
+        >
+          <div className={classes.photo}>
+            <img src={image} alt="profile" />
+          </div>
+        </Link>
+        <br></br>
+        <div className={classes.head}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", marginTop: "none" }}
+          >
+            <h1>Chan Wei Jie</h1>
+          </Link>
+          <p>
+            Front-end <br />
+            Back-end Developer
+          </p>
+          <br></br>
+        </div>
+        <div className={classes.a}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            About Me
+          </Link>
+        </div>
+        <div className={classes.a}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Experience
+          </Link>
+        </div>
+        <div className={classes.a}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Skills
+          </Link>
+        </div>
+        <div className={classes.a}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Education
+          </Link>
+        </div>
+        <div className={classes.last}>
+          <Link
+            to="/"
+            exact="true"
+            style={{ textDecoration: "none", color: "white" }}
+          >
+            Projects
+          </Link>
+        </div>
+
+        <br></br>
+        <div className={classes.social}>
+          <a href="mailto:chan_weijie@outlook.com" target="_blank">
+            <FaMailBulk size="30" color="#a4b3c4" className={classes.test} />
+          </a>
+
+          <a href="https://github.com/weijiepris" target="_blank">
+            <FaGithubSquare
+              size="30"
+              color="#a4b3c4"
+              className={classes.test}
+            />
+          </a>
+          <a href="https://www.linkedin.com/in/cweijie/" target="_blank">
+            <FaLinkedin size="30" color="#a4b3c4" className={classes.test} />
+          </a>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+}
+
+export default NavigationBar;
