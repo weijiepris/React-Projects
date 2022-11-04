f41342572cf4534291b8e5b5a8321c53f6641fb8
weijiepris <altxmare@hotmail.com>
2022-11-04 22:45:25 +0800

    added homepage

--- aubercot/src/components/common/HomePage.tsx
+++ aubercot/src/components/common/HomePage.tsx
@@ -2,10 +2,26 @@ import { Button } from "primereact/button";
 import { FC, useEffect, useState, useRef, useMemo, useContext } from "react";
 import { onLogout } from "../auth/actions";
 
+import "./HomePage.css";
 import AuthenticationContext from "../../store/authentication-content";
 
 const HomePage: FC = () => {
-  return <div></div>;
+  return (
+    <main className="default-container">
+      <section>
+        <h1>Dashboard</h1>
+      </section>
+      <section className="section-summary">
+        <div className="section-summary-grid">Sales</div>
+        <div className="section-summary-grid">Products</div>
+        <div className="section-summary-grid">Stocks</div>
+        <div className="section-summary-grid">Stocks</div>
+        <div className="section-summary-grid">Stocks</div>
+      </section>
+      <section className="section-charts"></section>
+      <section className="section-etc"></section>
+    </main>
+  );
 };
 
 export default HomePage;
