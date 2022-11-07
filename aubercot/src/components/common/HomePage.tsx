bb5a02ad931cdec55c8c9212b02a9271dab4e03f
weijiepris <altxmare@hotmail.com>
2022-11-07 23:26:00 +0800

    updated main page

--- aubercot/src/components/common/HomePage.tsx
+++ aubercot/src/components/common/HomePage.tsx
@@ -1,6 +1,8 @@
 import { Button } from "primereact/button";
 import { FC, useEffect, useState, useRef, useMemo, useContext } from "react";
 import { onLogout } from "../auth/actions";
+import { Tooltip } from "primereact/tooltip";
+import { Card } from "primereact/card";
 
 import "./HomePage.css";
 import AuthenticationContext from "../../store/authentication-content";
@@ -11,15 +13,19 @@ const HomePage: FC = () => {
       <section>
         <h1>Dashboard</h1>
       </section>
-      <section className="section-summary">
-        <div className="section-summary-grid">Sales</div>
-        <div className="section-summary-grid">Products</div>
-        <div className="section-summary-grid">Stocks</div>
-        <div className="section-summary-grid">Stocks</div>
-        <div className="section-summary-grid">Stocks</div>
+      <section className="section-summary section">
+        <div className="items">
+          <div>Products</div>
+          <div>0</div>
+        </div>
+        <div className="items" data-pr-tooltip={"test"}>
+          <div className="profits">Profits</div>
+          <div>$0.00</div>
+        </div>
       </section>
-      <section className="section-charts"></section>
-      <section className="section-etc"></section>
+      <section className="section-charts section"></section>
+      <section className="section-etc section"></section>
+      <Tooltip target=".items" mouseTrack mouseTrackLeft={10} />
     </main>
   );
 };
--- aubercot/src/components/common/Sidebar.tsx
+++ aubercot/src/components/common/Sidebar.tsx
@@ -14,24 +14,34 @@ const Sidebar: FC<Props> = ({}) => {
 
   return (
     <section className="sidebar-container">
-      <ul>
-        <li>
-          <span>
-            <i className="pi pi-chart-bar"></i>Dashboard
-          </span>
-        </li>
-        <li>
-          <span>
-            <i className="pi pi-box"></i>Inventory
-          </span>
-        </li>
-        <li>
-          <span>
-            <i className="pi pi-cloud-upload"></i>Scan
-          </span>
-        </li>
-      </ul>
-
+      <div className="sidebar-content">
+        <ul>
+          <li>
+            <span>
+              <i className="pi pi-chart-bar"></i>Dashboard
+            </span>
+          </li>
+          <li>
+            <span>
+              <i className="pi pi-box"></i>Inventory
+            </span>
+          </li>
+          <li>
+            <span>
+              <i className="pi pi-cloud-upload"></i>Scan
+            </span>
+          </li>
+          <li>
+            <span>
+              <i
+                className="pi pi-database
+"
+              ></i>
+              Sales
+            </span>
+          </li>
+        </ul>
+      </div>
       <Footer />
     </section>
   );
