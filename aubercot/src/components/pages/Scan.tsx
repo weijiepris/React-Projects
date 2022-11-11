de8c5ec29569a91ec59d02c06ccc81f3b2212ca4
weijiepris <altxmare@hotmail.com>
2022-11-11 09:33:03 +0800

    added data table for scan page

--- aubercot/src/components/pages/Scan.tsx
+++ aubercot/src/components/pages/Scan.tsx
@@ -1,192 +1,264 @@
-import { FC, useEffect, useState } from "react";
-import { FilterMatchMode, FilterOperator } from "primereact/api";
+//@ts-nocheck
+import { FC, useEffect, useState, useRef } from "react";
 import { DataTable } from "primereact/datatable";
 import { Column } from "primereact/column";
+import { Button } from "primereact/button";
+import { FileUpload } from "primereact/fileupload";
 import { InputText } from "primereact/inputtext";
-import { Dropdown } from "primereact/dropdown";
+import { Tooltip } from "primereact/tooltip";
+import { Toast } from "primereact/toast";
+import axios from "axios";
 
 interface Props {
   alert: Function;
 }
 const Scan: FC<Props> = () => {
-  const [customers, setCustomers] = useState(null);
-  const [filters1, setFilters1] = useState({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    name: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    "country.name": {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    representative: { value: null, matchMode: FilterMatchMode.IN },
-    status: {
-      operator: FilterOperator.OR,
-      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-    },
-  });
-  const [filters2, setFilters2] = useState({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    name: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    "country.name": {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    representative: { value: null, matchMode: FilterMatchMode.IN },
-    status: {
-      operator: FilterOperator.OR,
-      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-    },
-  });
-  const [filters3, setFilters3] = useState({
-    global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-    name: {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    "country.name": {
-      operator: FilterOperator.AND,
-      constraints: [{ value: null, matchMode: FilterMatchMode.STARTS_WITH }],
-    },
-    representative: { value: null, matchMode: FilterMatchMode.IN },
-    status: {
-      operator: FilterOperator.OR,
-      constraints: [{ value: null, matchMode: FilterMatchMode.EQUALS }],
-    },
-  });
-  const [selectedCustomer2, setSelectedCustomer2] = useState(null);
+  const [posts, setPosts] = useState([]);
+  const [products, setProducts] = useState([]);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [importedData, setImportedData] = useState([]);
+  const [selectedImportedData, setSelectedImportedData] = useState([]);
+  const [importedCols, setImportedCols] = useState([
+    { field: "", header: "Header" },
+  ]);
+  const [globalFilter, setGlobalFilter] = useState(null);
+  const dt = useRef(null);
+  const toast = useRef(null);
 
-  const statuses = [
-    "unqualified",
-    "qualified",
-    "new",
-    "negotiation",
-    "renewal",
-    "proposal",
+  const cols = [
+    { field: "id", header: "id" },
+    { field: "userId", header: "User ID" },
+    { field: "title", header: "Title" },
+    { field: "body", header: "Description" },
   ];
 
-  const filtersMap: any = {
-    filters1: { value: filters1, callback: setFilters1 },
-    filters2: { value: filters2, callback: setFilters2 },
-    filters3: { value: filters3, callback: setFilters3 },
+  const exportColumns = cols.map((col) => ({
+    title: col.header,
+    dataKey: col.field,
+  }));
+
+  useEffect(() => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/posts")
+      .then((res) => setPosts(res.data));
+  }, []);
+
+  const importCSV = (e: any) => {
+    const file = e.files[0];
+    const reader = new FileReader();
+    reader.onload = (e: any) => {
+      const csv = e.target.result;
+      const data = csv.split("\n");
+
+      // Prepare DataTable
+      const cols = data[0].replace(/['"]+/g, "").split(",");
+      data.shift();
+
+      let _importedCols = cols.map((col: any) => ({
+        field: col,
+        header: toCapitalize(col.replace(/['"]+/g, "")),
+      }));
+      let _importedData = data.map((d: any) => {
+        d = d.split(",");
+        return cols.reduce((obj: any, c: any, i: any) => {
+          obj[c] = d[i].replace(/['"]+/g, "");
+          return obj;
+        }, {});
+      });
+
+      setImportedCols(_importedCols);
+      setImportedData(_importedData);
+    };
+
+    reader.readAsText(file, "UTF-8");
   };
 
-  useEffect(() => {}, []);
+  const importExcel = (e: any) => {
+    const file = e.files[0];
 
-  const statusFilterTemplate = (options: any) => {
-    return (
-      <Dropdown
-        value={options.value}
-        options={statuses}
-        onChange={(e) => options.filterCallback(e.value, options.index)}
-        itemTemplate={statusItemTemplate}
-        placeholder="Select a Status"
-        className="p-column-filter"
-        showClear
+    import("xlsx").then((xlsx: any) => {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        const wb = xlsx.read(e.target.result, { type: "array" });
+        const wsname = wb.SheetNames[0];
+        const ws = wb.Sheets[wsname];
+        const data = xlsx.utils.sheet_to_json(ws, { header: 1 });
+
+        // Prepare DataTable
+        const cols = data[0];
+        data.shift();
+
+        let _importedCols = cols.map((col: any) => ({
+          field: col,
+          header: toCapitalize(col),
+        }));
+        let _importedData = data.map((d: any) => {
+          return cols.reduce((obj: any, c: any, i: any) => {
+            obj[c] = d[i];
+            return obj;
+          }, {});
+        });
+
+        setImportedCols(_importedCols);
+        setImportedData(_importedData);
+      };
+
+      reader.readAsArrayBuffer(file);
+    });
+  };
+
+  const exportCSV = (selectionOnly: any) => {
+    dt.current.exportCSV({ selectionOnly });
+  };
+
+  const exportPdf = () => {
+    import("jspdf").then((jsPDF: any) => {
+      import("jspdf-autotable").then(() => {
+        const doc = new jsPDF.default(0, 0);
+        doc.autoTable(exportColumns, posts);
+        doc.save("products.pdf");
+      });
+    });
+  };
+
+  const exportExcel = () => {
+    import("xlsx").then((xlsx) => {
+      const worksheet = xlsx.utils.json_to_sheet(posts);
+      const workbook = { Sheets: { data: worksheet }, SheetNames: ["data"] };
+      const excelBuffer = xlsx.write(workbook, {
+        bookType: "xlsx",
+        type: "array",
+      });
+      saveAsExcelFile(excelBuffer, "posts");
+    });
+  };
+
+  const saveAsExcelFile = (buffer: any, fileName: any) => {
+    import("file-saver").then((module) => {
+      if (module && module.default) {
+        let EXCEL_TYPE =
+          "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8";
+        let EXCEL_EXTENSION = ".xlsx";
+        const data = new Blob([buffer], {
+          type: EXCEL_TYPE,
+        });
+
+        module.default.saveAs(
+          data,
+          fileName + "_export_" + new Date().getTime() + EXCEL_EXTENSION
+        );
+      }
+    });
+  };
+
+  const toCapitalize = (s: any) => {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+  };
+
+  const clear = () => {
+    setImportedData([]);
+    setSelectedImportedData([]);
+    setImportedCols([{ field: "", header: "Header" }]);
+  };
+
+  const onImportSelectionChange = (e: any) => {
+    setSelectedImportedData(e.value);
+    const detail = e.value.map((d: any) => Object.values(d)[0]).join(", ");
+    toast.current.show({
+      severity: "info",
+      summary: "Data Selected",
+      detail,
+      life: 3000,
+    });
+  };
+
+  const onSelectionChange = (e: any) => {
+    setSelectedProducts(e.value);
+  };
+
+  const header = (
+    <div className="flex align-items-center export-buttons">
+      <Button
+        type="button"
+        icon="pi pi-file"
+        onClick={() => exportCSV(false)}
+        className="mr-2"
+        data-pr-tooltip="Export all to CSV"
       />
-    );
-  };
-
-  const statusItemTemplate = (option: any) => {
-    return <span className={`customer-badge status-${option}`}>{option}</span>;
-  };
-
-  const onGlobalFilterChange = (event: any, filtersKey: any) => {
-    const value = event.target.value;
-    let filters = { ...filtersMap[filtersKey].value };
-    filters["global"].value = value;
-
-    filtersMap[filtersKey].callback(filters);
-  };
-
-  const renderHeader = (filtersKey: any) => {
-    const filters = filtersMap[`${filtersKey}`].value;
-    const value = filters["global"] ? filters["global"].value : "";
-
-    return (
-      <span className="p-input-icon-left">
-        <i className="pi pi-search" />
-        <InputText
-          type="search"
-          value={value || ""}
-          onChange={(e) => onGlobalFilterChange(e, filtersKey)}
-          placeholder="Global Search"
-        />
-      </span>
-    );
-  };
-
-  const header2 = renderHeader("filters2");
+      <Button
+        type="button"
+        icon="pi pi-file-excel"
+        onClick={exportExcel}
+        className="p-button-success mr-2"
+        data-pr-tooltip="Export all to Excel"
+      />
+      <Button
+        type="button"
+        icon="pi pi-file-pdf"
+        onClick={exportPdf}
+        className="p-button-warning mr-2"
+        data-pr-tooltip="Export all to PDF"
+      />
+      <Button
+        type="button"
+        icon="pi pi-filter"
+        onClick={() => exportCSV(true)}
+        className="p-button-info ml-auto"
+        data-pr-tooltip="Export Selections Only"
+      />
+      <div className="table-header">
+        <h5 className="mx-0 my-1">Manage Products</h5>
+        <span className="p-input-icon-left">
+          <i className="pi pi-search" />
+          <InputText
+            type="search"
+            onInput={(e) => setGlobalFilter(e.target.value)}
+            placeholder="Search..."
+          />
+        </span>
+      </div>
+    </div>
+  );
 
   return (
-    <main className="section-container">
+    <div className="section-container">
       <section className="header">
         <h1>Scan</h1>
       </section>
-      <section className="section">
+      <div className="card">
+        <h5>Export</h5>
+
+        <Tooltip target=".export-buttons>button" position="bottom" />
+
         <DataTable
-          value={customers}
-          paginator
-          rows={10}
-          header={header2}
-          filters={filters2}
-          onFilter={(e: any) => setFilters2(e.filters)}
-          selection={selectedCustomer2}
-          onSelectionChange={(e) => setSelectedCustomer2(e.value)}
-          selectionMode="single"
+          ref={dt}
+          globalFilter={globalFilter}
+          value={posts}
+          header={header}
           dataKey="id"
           responsiveLayout="scroll"
-          stateStorage="local"
-          stateKey="dt-state-demo-local"
-          emptyMessage="No customers found."
+          paginatorTemplate="FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink CurrentPageReport RowsPerPageDropdown"
+          paginator
+          currentPageReportTemplate="Showing {first} to {last} of {totalRecords} posts"
+          rows={10}
+          className="datatable-responsive"
+          responsiveLayout="scroll"
+          selectionMode="multiple"
+          emptyMessage="No data found."
+          selection={selectedProducts}
+          onSelectionChange={onSelectionChange}
         >
-          <Column
-            field="name"
-            header="Product Name"
-            sortable
-            filter
-            filterPlaceholder="Search by name"
-          ></Column>
-          <Column
-            header="Country"
-            sortable
-            sortField="country.name"
-            filter
-            filterField="country.name"
-            filterPlaceholder="Search by country"
-          ></Column>
-          <Column
-            header="Date"
-            sortable
-            sortField="country.name"
-            filter
-            filterField="country.name"
-            filterPlaceholder="Search by country"
-          ></Column>
-          <Column
-            header="Remarks"
-            sortable
-            sortField="representative.name"
-            filter
-            filterField="representative"
-            filterMenuStyle={{ width: "14rem" }}
-          ></Column>
-          <Column
-            field="status"
-            header="Status"
-            sortable
-            filter
-            filterElement={statusFilterTemplate}
-            filterMenuStyle={{ width: "14rem" }}
-          ></Column>
+          {cols.map((col, index) => (
+            <Column
+              key={index}
+              field={col.field}
+              header={col.header}
+              sortable
+            />
+          ))}
         </DataTable>
-      </section>
-      <section className="section"></section>
-    </main>
+      </div>
+    </div>
   );
 };
 
